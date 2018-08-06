e53d3ed381f0b4fa666044d4e2c580d8fcebf00a
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2018-08-06 15:08:14 +0200

    add dynamodb put-item

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "development tooling for AWS"
-	app.Version = "0.1.3"
+	app.Version = "0.1.4"
 
 	app.Commands = []cli.Command{
 		dynamodbCommand(),
