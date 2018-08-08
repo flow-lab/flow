cfc4eb51b6ac07449fadf9cfaadf0f32e0e316ba
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2018-08-08 21:28:03 +0200

    update version
    Signed-off-by: Krzysztof Grodzicki <kgrodzicki@gmail.com>

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "development tooling for AWS"
-	app.Version = "0.1.10"
+	app.Version = "0.1.11"
 
 	app.Commands = []cli.Command{
 		dynamodbCommand(),
