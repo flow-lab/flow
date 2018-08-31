b7f6c21079a8d60c6a57ff57ba8628d625e51248
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2018-08-31 10:04:05 +0200

    bump: version to 0.1.23

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "development tooling for AWS"
-	app.Version = "0.1.22"
+	app.Version = "0.1.23"
 
 	app.Commands = []cli.Command{
 		dynamodbCommand(),
