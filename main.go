1e55eb6454dbf5d28f118757ee5356f3db797bac
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2018-12-06 15:25:25 +0100

    fix: aws profile for upload artifacts to s3 bucket

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "development tooling for AWS"
-	app.Version = "0.1.34"
+	app.Version = "0.1.35"
 
 	app.Commands = []cli.Command{
 		dynamodbCommand(),
