5450a539c788b2440ddb0908a5c93d6acd6d1a9f
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2019-05-06 14:58:49 +0200

    add: kafka get-bootstrap-brokers and describe-cluster

--- kafka.go
+++ kafka.go
@@ -22,7 +22,6 @@ var kafkaCommand = func() cli.Command {
 				Action: func(c *cli.Context) error {
 					profile := c.String("profile")
 					sess := NewSessionWithSharedProfile(profile)
-
 					kc := kafka.New(sess)
 
 					listClusterInput := kafka.ListClustersInput{}
@@ -39,6 +38,98 @@ var kafkaCommand = func() cli.Command {
 					return nil
 				},
 			},
+			{
+				Name:  "describe-cluster",
+				Usage: "",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "cluster-name",
+						Value: "",
+					},
+					cli.StringFlag{
+						Name:  "profile",
+						Value: "",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					profile := c.String("profile")
+					clusterName := c.String("cluster-name")
+					sess := NewSessionWithSharedProfile(profile)
+					kc := kafka.New(sess)
+
+					clusterArn, err := getClusterArn(clusterName, kc)
+					if err != nil {
+						return err
+					}
+
+					describeClusterInput := kafka.DescribeClusterInput{
+						ClusterArn: clusterArn,
+					}
+
+					describeClusterOutput, err := kc.DescribeCluster(&describeClusterInput)
+					if err != nil {
+						return err
+					}
+
+					fmt.Printf("%v", describeClusterOutput)
+
+					return nil
+				},
+			},
+			{
+				Name:  "get-bootstrap-brokers",
+				Usage: "",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "cluster-name",
+						Value: "",
+					},
+					cli.StringFlag{
+						Name:  "profile",
+						Value: "",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					profile := c.String("profile")
+					clusterName := c.String("cluster-name")
+					sess := NewSessionWithSharedProfile(profile)
+					kc := kafka.New(sess)
+
+					clusterArn, err := getClusterArn(clusterName, kc)
+					if err != nil {
+						return err
+					}
+
+					getBootstrapBrokersInput := kafka.GetBootstrapBrokersInput{
+						ClusterArn: clusterArn,
+					}
+
+					getBootstrapBrokersInputOutput, err := kc.GetBootstrapBrokers(&getBootstrapBrokersInput)
+					if err != nil {
+						return err
+					}
+
+					fmt.Printf("%v", getBootstrapBrokersInputOutput)
+
+					return nil
+				},
+			},
 		},
 	}
+}
+
+func getClusterArn(s string, kc *kafka.Kafka) (*string, error) {
+	listClusterInput := kafka.ListClustersInput{}
+	listClusterOutput, err := kc.ListClusters(&listClusterInput)
+	if err != nil {
+		return nil, fmt.Errorf("unable to list clusters: %v", err)
+	}
+
+	for _, cluster := range listClusterOutput.ClusterInfoList {
+		if s == *cluster.ClusterName {
+			return cluster.ClusterArn, nil
+		}
+	}
+
+	return nil, fmt.Errorf("cluster arn for %s not found", s)
 }
